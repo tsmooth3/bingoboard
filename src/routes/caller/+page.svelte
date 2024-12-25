7ffe3e2317271a6be5f8f91d9b6d75f8018a9bde
tsmooth3 <tstewart3@gmail.com>
2024-12-25 13:08:38 -0500

    caller page

---
+++ src/routes/caller/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { persisted } from "svelte-persisted-store";
+    import { stuffdadsays, type Cell } from "$lib/index";
+
+    const pickedPhrases = persisted<string[]>('pickedPhrases', []);
+
+    function pickPhrase(): void {
+        if (stuffdadsays.length === 0) return;
+
+        const randomIndex = Math.floor(Math.random() * stuffdadsays.length);
+        const pickedPhrase = stuffdadsays.splice(randomIndex, 1)[0];
+        pickedPhrases.update((phrases) => [pickedPhrase, ...phrases]);
+    }
+
+    function clearPhrases(): void {
+        pickedPhrases.set([]);
+    }
+
+    onMount(() => {
+        pickPhrase(); // Pick an initial phrase on mount
+    });
+</script>
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div class="space-y-10 text-center flex flex-col items-center">
+        <button class="btn btn-xl variant-ghost-primary" on:click={pickPhrase}>Pick a Phrase</button>
+        <div class="space-y-3">
+            {#each $pickedPhrases as phrase}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <div class="card break-words whitespace-normal variant-ghost-primary">
+                <section class="p-2">{phrase}</section>
+            </div>
+            {/each}
+        </div>
+        <button class="btn btn-xl variant-ghost-warning" on:click={clearPhrases}>Reset</button>
+    </div>
+</div>
